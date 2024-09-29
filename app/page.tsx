cbc325604112f74d3931fc1040a3b0585f21c571
rahat <rahat.hosen@trancombd.com>
2024-09-29 10:20:45 +0600

    up

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ import { GithubIcon } from "./_icons/other-icons";
 import Link from "next/link";
 import { siteConfig } from "@/lib/site-config";
 import HeaderNavigation from "@/components/core/headerNavigation";
+import BottomNavigation from "@/components/core/bottomNavigation";
 
 export default function Home() {
   return (
-    <main className="flex flex-auto flex-col items-center pt-24">
+   <>
+    <main>
       <HeaderNavigation/>
        <Link href={'/mobile_feature'} >Mobile Feature</Link>
+       <BottomNavigation/>
     </main>
+   </>
   );
 }
